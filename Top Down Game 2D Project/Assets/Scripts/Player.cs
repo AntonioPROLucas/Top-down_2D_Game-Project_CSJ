0530052f4cbc4727adc4a20ff03d83701f8a144a
AntonioPROLucas <106645196+AntonioPROLucas@users.noreply.github.com>
2023-12-30 00:22:48 -0300

    gameplay animation update
    -O personagem principal agora pode andar e correr (no leftShift), além de receber as respectivas animações!
    -Updates no mapa.
    
    ...
    
    -The main character can walk and run now, and now he gets the respective animations !
    -Map Updates

--- Top Down Game 2D Project/Assets/Scripts/Player.cs
+++ Top Down Game 2D Project/Assets/Scripts/Player.cs
@@ -4,24 +4,71 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    [SerializeField]private float speed;
+    [SerializeField]private float runSpeed;
 
+    private float initialSpeed;
     private Rigidbody2D rig;
-    private Vector2 direction;
+    private Vector2 _direction;
+    private bool _isRunning;
+
+    public Vector2 direction
+    {
+        get {return _direction;}
+        set {_direction = value;}
+    }
+    public bool isRunning
+    {
+        get {return _isRunning;}
+        set {_isRunning = value;}
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
     }
 
     // Update is called once per frame
     private void Update()
     {
-       direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) ;
+       OnInput();
+
+       OnRun();
     }
 
     private void FixedUpdate()
     {
+        OnMove();
+    }
+
+    #region Movement
+
+    void OnInput()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) ;
+    }
+
+    void OnMove()
+    {
         rig.MovePosition(rig.position + direction * speed * Time.fixedDeltaTime);
     }
+
+    void OnRun()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            _isRunning = false;
+        }
+    }
+
+    #endregion
 }
